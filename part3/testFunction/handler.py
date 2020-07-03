dbf90e9917581f662baeb6a756b1daf25cc50101
serj <d.serj1078@gmail.com>
2020-07-03 11:50:54 +0300

    new version

--- part3/testFunction/handler.py
+++ part3/testFunction/handler.py
@@ -1,9 +1,55 @@
 import json
 
+import nltk
+nltk.data.path=['.data']
+
+from nltk.corpus import stopwords
+from string import punctuation
+
+stopwords_eng = stopwords.words('english')
+
+def bag_of_words(words):
+    bag = {}
+    for w in words:
+        bag[w] = bag.get(w,0)+1
+    return bag
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import re
+# from nltk.util import ngrams
+from nltk.util import everygrams
+
+lemmatizer = WordNetLemmatizer()
+
+def extract_features(document):
+    words = word_tokenize(document)
+    lemmas = [str(lemmatizer.lemmatize(w)) for w in words if w not in stopwords_eng and w not in punctuation]
+    document = " ".join(lemmas)
+    document = document.lower()
+    document = re.sub(r'[^a-zA-Z0-9\s]', ' ', document)
+    words = [w for w in document.split(" ") if w!="" and w not in stopwords_eng and w not in punctuation]
+    return [str('_'.join(ngram)) for ngram in list(everygrams(words, max_len=3))]
+
+import pickle
+import sys
+
+model_file = open('sa_classifier.pickle', 'rb')
+model = pickle.load(model_file)
+model_file.close()
+
+from nltk.tokenize import word_tokenize
+
+def get_sentiment(review):
+    #words = word_tokenize(review)
+    words = extract_features(review)
+    words = bag_of_words(words)
+    return model.classify(words)
 
 def hello(event, context):
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "result": get_sentiment(event['review']),
         "input": event
     }
 
@@ -13,12 +59,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
